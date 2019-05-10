68e63a854ee11527308e30b61c629ccba2614710
Brian Alexander <balexand@gmail.com>
2019-05-10 17:42:59 -0600

    implement

--- lib/single.ex
+++ lib/single.ex
@@ -1,18 +1,70 @@
 defmodule Single do
   @moduledoc """
-  Documentation for Single.
+  Manager process that starts and watches a singleton process. This process should be started on
+  each node that the singleton process will be run on. The Erlang
+  [`:global` module](http://erlang.org/doc/man/global.html) is responsible for ensuring that there
+  are not duplicate instances of the singleton process within the cluster.
+
+  If the singleton process has not already been started then it will be started and linked to the
+  manager process. If the singleton process exits for any reason, whether `:normal` or otherwise,
+  then the `Single` process will exit with the same reason. If the manager process exits with a
+  reason other than `:normal` then the singleton process will exit with the same reason.
+
+  If the singleton process is already started then the manager process will monitor it. If the
+  remote singleton process exits for any reason then the manager process will try to start it
+  again.
   """
 
-  @doc """
-  Hello world.
+  use GenServer
 
-  ## Examples
+  defmodule State do
+    @moduledoc false
+    defstruct [:init_arg, :module, :name, :ref, :ref_type]
+  end
 
-      iex> Single.hello()
-      :world
+  def start(module, init_arg, name, options \\ []) do
+    state = %State{init_arg: init_arg, module: module, name: name}
+    GenServer.start(__MODULE__, state, options)
+  end
 
-  """
-  def hello do
-    :world
+  def start_link(module, init_arg, name, options \\ []) do
+    state = %State{init_arg: init_arg, module: module, name: name}
+    GenServer.start_link(__MODULE__, state, options)
+  end
+
+  ###
+  # GenServer callbacks
+  ###
+
+  @doc false
+  def init(%State{} = state) do
+    {:ok, start_child(state)}
+  end
+
+  @doc false
+  def handle_info({:DOWN, ref, :process, _, :normal}, %State{ref: ref, ref_type: :local} = state) do
+    # Local process exited normally. This process should do the same.
+    {:stop, :normal, state}
+  end
+
+  def handle_info({:DOWN, ref, :process, _, _reason}, %State{ref: ref, ref_type: :local} = state) do
+    # Local process exited with reason other than :normal. Process linking will take care of this.
+    {:noreply, state}
+  end
+
+  def handle_info({:DOWN, ref, :process, _, _reason}, %State{ref: ref, ref_type: :remote} = state) do
+    # Remote process exited. Try starting a new process.
+    {:noreply, start_child(state)}
+  end
+
+  @doc false
+  defp start_child(%State{init_arg: init_arg, module: module, name: name} = state) do
+    case GenServer.start_link(module, init_arg, name: {:global, name}) do
+      {:ok, pid} ->
+        %{state | ref: Process.monitor(pid), ref_type: :local}
+
+      {:error, {:already_started, pid}} ->
+        %{state | ref: Process.monitor(pid), ref_type: :remote}
+    end
   end
 end
--- test/single_test.exs
+++ test/single_test.exs
@@ -1,8 +1,118 @@
 defmodule SingleTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Single
 
-  test "greets the world" do
-    assert Single.hello() == :world
+  import ExUnit.CaptureLog
+
+  defmodule TestServer do
+    use GenServer
+
+    def get_state(pid) do
+      GenServer.call(pid, :get_state)
+    end
+
+    ###
+    # GenServer callbacks
+    ###
+    def init(state) do
+      {:ok, state}
+    end
+
+    def handle_call(:get_state, _from, state) do
+      {:reply, state, state}
+    end
+  end
+
+  test "test TestServer" do
+    {:ok, pid} = GenServer.start_link(TestServer, "it works")
+    assert TestServer.get_state(pid) == "it works"
+  end
+
+  test "starts managed process" do
+    {:ok, _pid} = Single.start_link(TestServer, "test state", :my_singleton)
+    assert TestServer.get_state({:global, :my_singleton}) == "test state"
+  end
+
+  test "with existing, unmanaged process" do
+    {:ok, existing_pid} =
+      GenServer.start_link(TestServer, "existing", name: {:global, :my_singleton})
+
+    {:ok, _pid} = Single.start_link(TestServer, "new", :my_singleton)
+
+    assert :global.whereis_name(:my_singleton) == existing_pid
+    assert TestServer.get_state({:global, :my_singleton}) == "existing"
+
+    # Stop the existing process and the new singleton will be started
+    GenServer.stop(existing_pid)
+    assert :global.whereis_name(:my_singleton) == :undefined
+    :timer.sleep(100)
+    assert TestServer.get_state({:global, :my_singleton}) == "new"
+  end
+
+  test "failover from one Single to another" do
+    {:ok, _pid} = Single.start_link(TestServer, "first", :my_singleton)
+    {:ok, _pid} = Single.start_link(TestServer, "second", :my_singleton)
+
+    assert TestServer.get_state({:global, :my_singleton}) == "first"
+    GenServer.stop({:global, :my_singleton})
+    :timer.sleep(100)
+
+    assert TestServer.get_state({:global, :my_singleton}) == "second"
+  end
+
+  test "shutting down Single shuts down managed process" do
+    {:ok, parent} = Single.start(TestServer, "first", :the_process)
+    pid = :global.whereis_name(:the_process)
+    ref = Process.monitor(pid)
+
+    GenServer.stop(parent, :shutdown)
+
+    assert_receive {:DOWN, ^ref, :process, ^pid, :shutdown}
+  end
+
+  test "stopping managed process with :normal stops Single with same reason" do
+    {:ok, pid} = Single.start_link(TestServer, "first", :the_process)
+    ref = Process.monitor(pid)
+    GenServer.stop({:global, :the_process}, :normal)
+
+    assert_receive {:DOWN, ^ref, :process, ^pid, :normal}
+  end
+
+  test "stopping managed process with :shutdown stops Single with same reason" do
+    {:ok, pid} = Single.start(TestServer, "first", :the_process)
+    ref = Process.monitor(pid)
+    GenServer.stop({:global, :the_process}, :shutdown)
+
+    assert_receive {:DOWN, ^ref, :process, ^pid, :shutdown}
+  end
+
+  test "stopping managed process with :kill stops Single with same reason" do
+    {:ok, pid} = Single.start(TestServer, "first", :the_process)
+    ref = Process.monitor(pid)
+
+    log =
+      capture_log(fn ->
+        GenServer.stop({:global, :the_process}, :kill)
+      end)
+
+    assert log =~ "[error] GenServer :the_process terminating"
+    assert log =~ "** (stop) :kill"
+
+    assert_receive({:DOWN, ^ref, :process, ^pid, :kill})
+  end
+
+  test "stopping managed process with :custom_reason stops Single with same reason" do
+    {:ok, pid} = Single.start(TestServer, "first", :the_process)
+    ref = Process.monitor(pid)
+
+    log =
+      capture_log(fn ->
+        GenServer.stop({:global, :the_process}, :custom_reason)
+      end)
+
+    assert log =~ "[error] GenServer :the_process terminating"
+    assert log =~ "** (stop) :custom_reason"
+
+    assert_receive({:DOWN, ^ref, :process, ^pid, :custom_reason})
   end
 end
