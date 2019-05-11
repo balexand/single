72429f7883b63ceb6a115a925ee37d93949696f7
Brian Alexander <balexand@gmail.com>
2019-05-10 18:12:35 -0600

    prepare to publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Single.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Runs supervised, singleton processes within an Erlang cluster.",
+      package: package()
     ]
   end
 
@@ -23,6 +25,13 @@ defmodule Single.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/balexand/single"}
     ]
   end
 end
