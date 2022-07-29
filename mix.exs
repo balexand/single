16b45bc23ffebea0c82b6d39407b021dff29c0a9
Brian Alexander <balexand@gmail.com>
2022-07-28 18:36:39 -0600

    CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Single.MixProject do
     [
       app: :single,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Runs supervised, singleton processes within an Erlang cluster.",
@@ -23,6 +23,8 @@ defmodule Single.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # Dev/test
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
