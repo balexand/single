1a57feaa6761adf234b108566c61744297fda960
Brian Alexander <balexand@gmail.com>
2022-07-28 19:07:36 -0600

    Update single_test.exs

--- test/single_test.exs
+++ test/single_test.exs
@@ -18,11 +18,30 @@ defmodule SingleTest do
     def handle_call(:ping, _from, state) do
       {:reply, :pong, state}
     end
+
+    def handle_call({:stop, reason}, _from, state) do
+      {:stop, reason, :ok, state}
+    end
   end
 
   test "starts child GenServer" do
     assert {:ok, _pid} = Single.start_link(ExampleServer, :thearg, :example)
 
     assert GenServer.call({:global, :example}, :ping) == :pong
+  end
+
+  test "manager exits with :normal when child process exits with :normal" do
+    assert {:ok, pid} = Single.start_link(ExampleServer, :thearg, :example)
+
+    child_pid = :global.whereis_name(:example)
+    assert Process.alive?(pid)
+    assert Process.alive?(child_pid)
+
+    Process.monitor(pid)
+
+    assert GenServer.call({:global, :example}, {:stop, :normal}) == :ok
+    refute Process.alive?(child_pid)
+
+    assert_receive {:DOWN, _ref, :process, ^pid, :normal}
   end
 end
