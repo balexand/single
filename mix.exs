17af0ae0460475c3c54db220b6d1e51fa3d56b8c
Brian Alexander <balexand@gmail.com>
2022-07-28 19:44:42 -0600

    bump version

--- mix.exs
+++ mix.exs
@@ -1,15 +1,25 @@
 defmodule Single.MixProject do
   use Mix.Project
 
+  @version "0.2.0"
+
   def project do
     [
       app: :single,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Runs supervised, singleton processes within an Erlang cluster.",
-      package: package()
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/balexand/single"}
+      ],
+      docs: [
+        extras: ["README.md"],
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/balexand/single"
+      ]
     ]
   end
 
@@ -26,13 +36,6 @@ defmodule Single.MixProject do
       # Dev/test
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev}
-    ]
-  end
-
-  defp package do
-    [
-      licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/balexand/single"}
     ]
   end
 end
